589adc607670fe3bef9aa973f3b19563eda89755
Sithe Ncube <me.sithe.ncube@gmail.com>
2019-06-25 22:56:31 +0200

    Demo version

--- CompilerAssignment/Program.cs
+++ CompilerAssignment/Program.cs
@@ -7,16 +7,16 @@ namespace CompilerAssignment
         static void Main(string[] args)
         {
             //Testing If Command
-            //Parser P = new Parser("if a + b then a := b + b else a := c + d");
+            //Parser P = new Parser("if a + b then a := b * e else a := c + d");
 
             //Testing Assign Command
             //Parser P = new Parser("a := b + c");
 
             //Testing Let Command
-            //Parser P = new Parser("let a in b := a + b");
+            Parser P = new Parser("let a in b := a + c");
 
             //Testing nested commands
-            Parser P = new Parser("let b in if a + b then let a in b := a + b else a := c + d");
+            //Parser P = new Parser("let b in if a + b then let a in b := a + b else a := c + d");
 
             Console.ReadLine();
         }
